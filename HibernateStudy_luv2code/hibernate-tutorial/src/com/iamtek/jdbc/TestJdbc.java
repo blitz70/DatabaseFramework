39e7fa7771b1ed1c9dc41cd771b05217f14f1a67
blitz-out <blitz70@hanmail.net>
2016-10-14 18:35:08 +0900

    Hibernate study added
    Setup

---
+++ HibernateStudy_luv2code/hibernate-tutorial/src/com/iamtek/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.iamtek.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		//DB connection info
+		String jdbcUrl = "jdbc:mysql://localhost:3306/mydb?useSSL=false";
+		String username = "blitz";
+		String password = "mysql";
+		
+		try {
+			System.out.println("Connecting to db: " + jdbcUrl);
+			Connection conn =
+					DriverManager.getConnection(jdbcUrl, username, password);
+			System.out.println("Connection success!");
+		} catch (Exception e) {
+			System.out.println("Connection failed!");
+			e.printStackTrace();
+		}
+	}
+
+}
